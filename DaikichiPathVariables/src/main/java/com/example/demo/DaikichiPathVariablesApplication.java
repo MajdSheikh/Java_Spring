7dfe34455054e50e7d8a659b78e9b4b4cbd753fc
Majd Sheikh <majd_sheikh@hotmail.com>
2022-11-01 05:11:15 +0200

    Daikichi Path Variables

---
+++ DaikichiPathVariables/src/main/java/com/example/demo/DaikichiPathVariablesApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+
+@RequestMapping("/daikichi")
+public class DaikichiPathVariablesApplication {
+
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city ;
+	}
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		if(num%2==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
+
+}
